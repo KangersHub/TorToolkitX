e709736d8876f1cbc2cc0d0a73d1b97f6a7cb2d4
Vijay Adithya <89298602+Vijayadithya003@users.noreply.github.com>
2022-03-21 17:46:26 +0530

    Update ExecVarsSample.py

--- tortoolkit/consts/ExecVarsSample.py
+++ tortoolkit/consts/ExecVarsSample.py
@@ -46,7 +46,7 @@ except:
         # DATABASE_URL = "dbname=tortk user=postgres password=your-pass host=db port=5432"
 
         # MEGA CONFIG
-        MEGA_ENABLE = False
+        MEGA_ENABLE = True
         MEGA_API = "5FS1vUr7h_ctqk_CvTzhzg"
         MEGA_UNAME = "Vijay Adithya"
         MEGA_PASS = "Adithya3300"
